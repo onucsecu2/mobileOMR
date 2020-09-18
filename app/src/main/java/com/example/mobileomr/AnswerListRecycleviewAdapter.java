c51d5270f7d3a485b09993639440dadb21e1eb7c
onucsecu2 <onucsecu@gmail.com>
2020-09-18 16:39:15 +0600

    answer saving sqlite

---
+++ app/src/main/java/com/example/mobileomr/AnswerListRecycleviewAdapter.java
@@ -0,0 +1,25 @@
+package com.example.mobileomr;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class AnswerListRecycleviewAdapter  extends RecyclerView.Adapter<RecyclerView.ViewHolder>  {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/example/mobileomr/SavedAnswerActivity.java
+++ app/src/main/java/com/example/mobileomr/SavedAnswerActivity.java
@@ -1,14 +1,19 @@
 package com.example.mobileomr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class SavedAnswerActivity extends AppCompatActivity {
-
+    private RecyclerView ans_RecycleView;
+    private AnswerListRecycleviewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_answer);
+        ans_RecycleView=(RecyclerView)findViewById(R.id.answer_recycleview);
+
+
     }
 }
